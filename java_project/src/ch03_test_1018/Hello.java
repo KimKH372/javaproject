b7a2bc1ff857f9f5217f0857de63128afef5ab31
KimKH372 <kkh9372@gmail.com>
2022-11-24 12:29:15 +0900

    dd

---
+++ java_project/src/ch03_test_1018/Hello.java
@@ -0,0 +1,23 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		// 실행은 ctrl + f11
+//		주석은 보통 ctrl + / : 단일 행 주석_ 한번 더 하면 취소
+//		System.out.println("Hello 자바");
+//		여러 행 주석 : ctrl + shift + /
+//		여러 행 주석 해제: ctrl + shift + \
+		/*
+		 * System.out.println("Hello 자바");
+		 *  System.out.println("Hello 자바");
+		 * System.out.println("Hello 자바");
+		 */
+//		자동 포맷 정렬 : ctrl + shift + f
+		System.out.println("Hello 자바");
+		System.out.println("Hello 자바");
+	}
+	
+
+}
